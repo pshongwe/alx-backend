a0379d4b0cf8216542ea20fc94aeb421ff6a4b74
pshongwe <piboman@yahoo.com>
2024-06-27 00:43:29 +0000

    update

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -24,7 +24,7 @@ class MRUCache(BaseCaching):
                 self.cache_data.move_to_end(key)
             self.cache_data[key] = item
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                discarded_key, _ = self.cache_data.popitem(last=True)
+                discarded_key, _ = self.cache_data.popitem(last=False)
                 print("DISCARD: {}".format(discarded_key))
 
     def get(self, key):
@@ -34,34 +34,3 @@ class MRUCache(BaseCaching):
             return None
         self.cache_data.move_to_end(key)
         return self.cache_data[key]
-
-
-if __name__ == "__main__":
-    MRUCache = __import__('4-mru_cache').MRUCache
-
-    my_cache = MRUCache()
-    my_cache.put("A", "Hello")
-    my_cache.put("B", "World")
-    my_cache.put("C", "Holberton")
-    my_cache.put("D", "School")
-    my_cache.print_cache()
-    print(my_cache.get("B"))
-    my_cache.put("E", "Battery")
-    my_cache.print_cache()
-    my_cache.put("C", "Street")
-    my_cache.print_cache()
-    print(my_cache.get("A"))
-    print(my_cache.get("B"))
-    print(my_cache.get("C"))
-    my_cache.put("F", "Mission")
-    my_cache.print_cache()
-    my_cache.put("G", "San Francisco")
-    my_cache.print_cache()
-    my_cache.put("H", "H")
-    my_cache.print_cache()
-    my_cache.put("I", "I")
-    my_cache.print_cache()
-    my_cache.put("J", "J")
-    my_cache.print_cache()
-    my_cache.put("K", "K")
-    my_cache.print_cache()
